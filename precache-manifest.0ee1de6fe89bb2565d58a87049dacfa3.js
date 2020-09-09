7fc6a7f0372dbf7dab43abe94489aa302d976d2d
ihor <malishkoio@gmail.com>
2020-09-10 02:27:33 +0300

    Updates

---
+++ precache-manifest.0ee1de6fe89bb2565d58a87049dacfa3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5426da2feecb4f8b617ddfd2d53489de",
+    "url": "/name/index.html"
+  },
+  {
+    "revision": "04ea3048bc07b8c42f7a",
+    "url": "/name/static/js/2.f40230db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/name/static/js/2.f40230db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5366c1e2dd7953ad7ea7",
+    "url": "/name/static/js/main.ad7583b6.chunk.js"
+  },
+  {
+    "revision": "967d9001410220c52eef",
+    "url": "/name/static/js/runtime-main.69f71a02.js"
+  }
+]);
